d61ac989ef2107a90e903073e9da803b34aa2e5b
Ian Garcez <ian@onespace.com.br>
2016-09-22 16:18:19 -0300

    change import_database messages to english

--- docker/scripts/import_database.sh
+++ docker/scripts/import_database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-  echo "forneça o id do container"
+  echo "Please provide container ID (use docker ps to find out the id of the mysql:latest container)"
   exit 1
 fi
 
@@ -9,12 +9,13 @@ then
   FILE=./schema.tar.gz
   tar -zxvf $FILE
   FILE_SQL=./schema.sql
-  echo "$FILE_SQL extraido, removendo"
+  echo "$FILE_SQL extracted, sending to container"
   /usr/bin/docker cp $FILE_SQL $1:/var/tmp/$FILE_SQL
   /usr/bin/docker exec $1 sh -c 'exec /usr/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/tmp/schema.sql'
   /usr/bin/docker exec $1 sh -c 'exec /bin/rm /var/tmp/schema.sql'
+  echo "Sent to container, removing local file"
   rm $FILE_SQL
 else
-   echo "schema.tar.gz ausente, não há banco para recuperar"
+   echo "./schema.tar.gz not found"
    exit 1
 fi
