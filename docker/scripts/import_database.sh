777f4cfa087346f328b800dfab4691f6080ae2a2
Ian Garcez <ian@onespace.com.br>
2016-11-01 10:25:09 -0300

    Change import script to use root user
    This will make mysql dumps that contains trigger and view statements to import successfully

--- docker/scripts/import_database.sh
+++ docker/scripts/import_database.sh
@@ -11,7 +11,7 @@ then
   FILE_SQL=./schema.sql
   echo "$FILE_SQL extracted, sending to container"
   /usr/bin/docker cp $FILE_SQL $1:/var/tmp/$FILE_SQL
-  /usr/bin/docker exec $1 sh -c 'exec /usr/bin/mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/tmp/schema.sql'
+  /usr/bin/docker exec $1 sh -c 'exec /usr/bin/mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /var/tmp/schema.sql'
   /usr/bin/docker exec $1 sh -c 'exec /bin/rm /var/tmp/schema.sql'
   echo "Sent to container, removing local file"
   rm $FILE_SQL
