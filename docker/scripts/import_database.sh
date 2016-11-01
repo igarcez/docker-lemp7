7a592f65d17ea9c261b650edb46a06189d989f32
Ian Garcez <ian@onespace.com.br>
2016-11-01 10:29:55 -0300

    Grab docker executable path dynamically
    This deals with docker executable path being different from system to system

--- docker/scripts/import_database.sh
+++ docker/scripts/import_database.sh
@@ -4,15 +4,17 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+DOCKER=$(which docker)
+
 if [ -f './schema.tar.gz' ];
 then
   FILE=./schema.tar.gz
   tar -zxvf $FILE
   FILE_SQL=./schema.sql
   echo "$FILE_SQL extracted, sending to container"
-  /usr/bin/docker cp $FILE_SQL $1:/var/tmp/$FILE_SQL
-  /usr/bin/docker exec $1 sh -c 'exec /usr/bin/mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /var/tmp/schema.sql'
-  /usr/bin/docker exec $1 sh -c 'exec /bin/rm /var/tmp/schema.sql'
+  $DOCKER cp $FILE_SQL $1:/var/tmp/$FILE_SQL
+  $DOCKER exec $1 sh -c 'exec /usr/bin/mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /var/tmp/schema.sql'
+  $DOCKER exec $1 sh -c 'exec /bin/rm /var/tmp/schema.sql'
   echo "Sent to container, removing local file"
   rm $FILE_SQL
 else
